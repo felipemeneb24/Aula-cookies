82bd54ace52412c2152d12f90092790c31519fed
Desktop <felipemeneghettib23@gmail.com>
2024-06-02 13:01:08 -0300

    Concluindo App Cadastro de Produtos

--- index.js
+++ index.js
@@ -19,38 +19,35 @@ app.use(session({
 }));
 
 app.use(cookieParser());// habilita o modulo cookie-parser. Mais um Middleware para permitir que a aplicaçao manipule cookies.
-let listaEmpresas = [];
+let listaProdutos = [];
 //manipular o express para manipular corretamendo os dados quando forem submetidos via post
 app.use(express.urlencoded({ extended: true })); //habilita a biblioca query string 
 import path from 'path';
 
 
-function cadastrarEmpresas(requisicao, resposta) {
-    const nome = requisicao.body.nome;
-    const fantasia = requisicao.body.fantasia;
-    const endereço = requisicao.body.endereço;
-    const cidade = requisicao.body.cidade;
-    const estado = requisicao.body.estado;
-    const cep = requisicao.body.cep;
-    const telefone = requisicao.body.telefone;
-    const cnpj = requisicao.body.cnpj;
-    const email = requisicao.body.email;
+function cadastrarProdutos(requisicao, resposta) {
+    const descricao = requisicao.body.descricao;
+    const fabricante = requisicao.body.fabricante;
+    const codbarras = requisicao.body.codbarras;
+    const estoque = requisicao.body.estoque;
+    const custo = requisicao.body.custo;
+    const venda = requisicao.body.venda;
+    const validade = requisicao.body.validade;
+
 
     //verificando se todos os campos estao prenchidos.
 
-    if (nome && fantasia && endereço && cidade && estado && cep && telefone && cnpj && email) {
-        listaEmpresas.push({
-            nome: nome,
-            fantasia: fantasia,
-            endereço: endereço,
-            cidade: cidade,
-            estado: estado,
-            cep: cep,
-            telefone: telefone,
-            cnpj: cnpj,
-            email: email,
+    if (descricao && fabricante && codbarras && estoque && custo && venda && validade) {
+        listaProdutos.push({
+            descricao: descricao,
+            fabricante: fabricante,
+            codbarras: codbarras,
+            estoque: estoque,
+            custo: custo,
+            venda: venda,
+            validade: validade
         });
-        resposta.redirect('/ListarEmpresas');
+        resposta.redirect('/ListarProdutos');
     }
 
     else {
@@ -65,137 +62,115 @@ function cadastrarEmpresas(requisicao, resposta) {
                 integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         </head>
         <body>
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="CadastrodeProdutos.html">Cadastro de Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/listarProdutos">Relatorio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/logout">Sair</a>
+                  </li>
+            </ul>
+            <br />
+            <h3 style="text-align: center;">Aplicação Web Cadastro de Produtos</h3>
             <div class="container" style="border: solid 2px; padding: 15px;">
-                <form method="post" action="/CadastrodeEmpresas" class="row g-3 needs-validation" novalidate>
+                <form method="post" action="/CadastrodeProdutos" class="row g-3 needs-validation" novalidate>
                     <div class="col-md-4">
-                        <label for="validationCustom01" class="form-label">Nome ou razão social</label>
-                        <input type="text" class="form-control" id="validationCustom01" name="nome" value="${nome}" required>`);
+                        <label for="validationCustom01" class="form-label">Descrição do Produto:</label>
+                        <input type="text" class="form-control" id="validationCustom01" name="descricao" value="${descricao}" required>`);
 
-        if (nome == "") {
+        if (descricao == "") {
             resposta.write(`<div class="alert alert-danger" role="alert">
-                             Por favor, preencha o campo Nome ou razão social
+                             Por favor, preencha a Descrição do Produto !
                             </div>`);
         }
 
-        resposta.write(`
+        resposta.write(`    
                     </div>
                     <div class="col-md-4">
-                        <label for="validationCustom02" class="form-label">Nome Fantasia</label>
-                        <input type="text" class="form-control" id="validationCustom02" name="fantasia" value="${fantasia}" required>`);
-        if (fantasia == "") {
-            resposta.write(`<div class="alert alert-danger" role="alert">
-                Por favor, preencha o campo Nome Fantasia
-              </div>`);
+                    <label for="validationCustom02" class="form-label">Nome do Fabricante:</label>
+                    <input type="text" class="form-control" id="validationCustom02" name="fabricante" value="${fabricante}" required>`);
+        if (fabricante == "") {
+            resposta.write(`
+                        <div class="alert alert-danger" role="alert">
+                        Por favor, preencha o Nome do Fabricante!
+                        </div>`);
         }
         resposta.write(`
                     </div>
-                    <div class="col-md-4">
-                        <label for="validationCustomUsername" class="form-label">Endereço</label>
+                        <div class="col-md-4">
+                         <label for="validationCustomUsername" class="form-label">Codigo de Barras:</label>
                         <div class="input-group has-validation">
-                        <input type="text" class="form-control" id="validationCustomUsername"
-                        aria-describedby="inputGroupPrepend" name="endereço" value="${endereço}" required>`);
-        if (endereço == "") {
+                        <input type="number" class="form-control" id="validationCustomUsername"
+                        aria-describedby="inputGroupPrepend" name="codbarras" value="${codbarras}" required>`);
+        if (codbarras == "") {
             resposta.write(`<div class="alert alert-danger" role="alert">
-            Por favor, preencha o campo Endereço
-          </div>`);
+                             Por favor, preencha o Codigo de Barras!
+                            </div>`);
         }
         resposta.write(`
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <label for="validationCustom03" class="form-label">Cidade</label>
-                    <input type="text" class="form-control" id="validationCustom03" name="cidade" value="${cidade}" required>`);
-        if (cidade == "") {
-            resposta.write(`<div class="alert alert-danger" role="alert">
-            Por favor, preencha o campo Cidade
-                </div>`);
+                            </div>
+                     </div>
+                     <div class="col-md-6">
+                     <label for="validationCustom03" class="form-label">Quantidade em Estoque:</label>
+                     <input type="number" class="form-control" id="validationCustom03" name="estoque" value="${estoque}" required>`);
+        if (estoque == "") {
+            resposta.write(`
+                    <div class="alert alert-danger" role="alert">
+                         Por favor, preencha a quantidade em Estoque!
+                      </div>`);
         }
         resposta.write(`</div>
-        <div class="col-md-3">
-            <label for="validationCustom04" class="form-label">Estado</label>
-            <select class="form-select" id="validationCustom04" name="estado" required>
-                <option selected disabled value="${estado}">Selecione um estado...</option>
-                <option value="AC">Acre</option>
-                <option value="AL">Alagoas</option>
-                <option value="AP">Amapá</option>
-                <option value="AM">Amazonas</option>
-                <option value="BA">Bahia</option>
-                <option value="CE">Ceará</option>
-                <option value="DF">Distrito Federal</option>
-                <option value="ES">Espírito Santo</option>
-                <option value="GO">Goiás</option>
-                <option value="MA">Maranhão</option>
-                <option value="MT">Mato Grosso</option>
-                <option value="MS">Mato Grosso do Sul</option>
-                <option value="MG">Minas Gerais</option>
-                <option value="PA">Pará</option>
-                <option value="PB">Paraíba</option>
-                <option value="PR">Paraná</option>
-                <option value="PE">Pernambuco</option>
-                <option value="PI">Piauí</option>
-                <option value="RJ">Rio de Janeiro</option>
-                <option value="RN">Rio Grande do Norte</option>
-                <option value="RS">Rio Grande do Sul</option>
-                <option value="RO">Rondônia</option>
-                <option value="RR">Roraima</option>
-                <option value="SC">Santa Catarina</option>
-                <option value="SP">São Paulo</option>
-                <option value="SE">Sergipe</option>
-                <option value="TO">Tocantins</option>
-            </select>`);
-        if (!estado) {
-            resposta.write(`<div class="alert alert-danger" role="alert">
-                    Por favor, preencha o campo Estado
-                  </div>`);
+            
+                     <div class="col-md-3">
+                      <label for="validationCustom05" class="form-label">Preço de Custo R$:</label>
+                      <input type="number" class="form-control" id="validationCustom05" name="custo" value="${custo}" required>`);
+        if (custo == "") {
+            resposta.write(`
+                        <div class="alert alert-danger" role="alert">
+                        Por favor, preencha o preço de Custo!
+                        </div>`);
         }
         resposta.write(` </div>
-        <div class="col-md-3">
-            <label for="validationCustom05" class="form-label">CEP</label>
-            <input type="text" class="form-control" id="validationCustom05" name="cep" value="${cep}" required>`);
-        if (cep == "") {
-            resposta.write(`<div class="alert alert-danger" role="alert">
-                Por favor, preencha o campo Cep
-              </div>`);
+                        <div class="col-md-3">
+                        <label for="validationCustom05" class="form-label">Preço de Venda R$:</label>
+                        <input type="number" class="form-control" id="validationCustom05" name="venda" value="${venda}" required>`);
+        if (venda == "") {
+            resposta.write(`
+                         <div class="alert alert-danger" role="alert">
+                            Por favor, preencha o preço de Venda!
+                        </div>`);
         }
-        resposta.write(`</div>
-        <div class="col-md-3">
-            <label for="validationCustom05" class="form-label">Telefone</label>
-            <input type="number" class="form-control" id="validationCustom05" name="telefone" placeholder="(XX) XXXXX-XXXX" value="${telefone}" required>`);
-        if (telefone == "") {
-            resposta.write(`<div class="alert alert-danger" role="alert">
-            Por favor, preencha o campo Telefone
-          </div>`);
+        resposta.write(`
+                        </div>
+                        <div class="col-md-3">
+                        <label for="validationCustom05" class="form-label">Data Validade:</label>
+                        <input type="text" class="form-control" id="validationCustom05" name="validade" placeholder="xx/xx/xx" value="${validade}" maxlength="8">`);
+        if (validade == "") {
+            resposta.write(`
+                        <div class="alert alert-danger" role="alert">
+                        Por favor, preencha a Validade do Produto!
+                        </div>`);
         }
-        resposta.write(`</div>
-        <div class="col-md-3">
-            <label for="validationCustom05" class="form-label">CNPJ</label>
-            <input type="number" class="form-control" id="validationCustom05" name="cnpj" value="${cnpj}" required>`);
-        if (cnpj == "") {
-            resposta.write(`<div class="alert alert-danger" role="alert">
-                Por favor, preencha o campo CNPJ
-              </div>`);
-        }
-        resposta.write(`</div>
-        <div class="col-md-3">
-            <label for="validationCustom05" class="form-label">Email</label>
-            <input type="text" class="form-control" id="validationCustom05" name="email" value="${email}" required>`);
-        if (email == "") {
-            resposta.write(`<div class="alert alert-danger" role="alert">
-                    Por favor, preencha o campo Email
-                  </div>`);
-        }
-        resposta.write(`</div>
-                            <div class="col-12">
-                            <button class="btn btn-primary" type="submit">Cadastrar Empresa</button>
-                         </div>            
-    </form>
+     
+        resposta.write(`
                 </div>
-                </body>
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-                integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-                crossorigin="anonymous"></script>
+                <div class="col-12">
+                    <button class="btn btn-primary" type="submit">Cadastrar Produto</button>
+                </div>
+          </form>
+         </div>
+        </body>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
 
-    </html>`);
+        </html>`);
 
 
 
@@ -207,11 +182,11 @@ function cadastrarEmpresas(requisicao, resposta) {
 function autenticarUsuario(requisicao, resposta) {
     const usuario = requisicao.body.usuario;
     const senha = requisicao.body.senha;
-    if (usuario == 'felipe' && senha == '03204') {
+    if (usuario == 'felipe' && senha == '03204' || usuario == 'admin' && senha == 'admin') {
         requisicao.session.usuarioAutenticado = true;
         resposta.cookie('dataUltimoAcesso', new Date().toLocaleString(), {
             httpOnly: true,
-            maxAge: 1000 * 60 * 15 * 24 *30
+            maxAge: 1000 * 60 * 15 * 24 * 30
         });
         resposta.redirect('/');
     }
@@ -226,7 +201,7 @@ function autenticarUsuario(requisicao, resposta) {
         resposta.write('<h1>Usuário ou senha inválidos</h1>');
         resposta.write('<p>Por favor, tente novamente</p>');
         resposta.write('<p><a href="/login.html">Voltar</a></p>');
-        if(requisicao.cookies.dataUltimoAcesso){
+        if (requisicao.cookies.dataUltimoAcesso) {
             resposta.write('<p>Ultimo acesso: ' + requisicao.cookies.dataUltimoAcesso + '</p>');
         }
         resposta.write('</body>');
@@ -246,49 +221,46 @@ app.get('/logout', (req, resp) => {
 });
 
 
-//quando um usuario enviar uma requisiçao do tipo post para o end point /cadastrodeEmpresas acionar a funçao cadastrarEmpresas.
-app.post('/CadastrodeEmpresas', usuarioAutenticado, cadastrarEmpresas)
+//quando um usuario enviar uma requisiçao do tipo post para o end point /cadastrodeProdutos acionar a funçao cadastrarProdutos.
+app.post('/CadastrodeProdutos', usuarioAutenticado, cadastrarProdutos)
 
-app.get('/listarEmpresas', usuarioAutenticado, (req, resp) => {
+app.get('/listarProdutos', usuarioAutenticado, (req, resp) => {
     resp.write('<!DOCTYPE html>');
     resp.write('<html>');
     resp.write('<head>');
     resp.write('<meta charset="UTF-8">');
-    resp.write('<title>Lista de Empresas</title>');
+    resp.write('<title>Lista de Produtos</title>');
     resp.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">')
     resp.write('</head>');
     resp.write('<body>');
-    resp.write('<h3>Empresas Cadastradas</h3>');
+    resp.write('<h3>Produtos Cadastrados</h3>');
     resp.write('<table class="table table-dark table-striped">');
     resp.write('<tr>');
-    resp.write('<th>Nome ou Razao Social</th>');
-    resp.write('<th>Nome Fantasia</th>');
-    resp.write('<th>Endereço</th>');
-    resp.write('<th>Cidade</th>');
-    resp.write('<th>Estado</th>');
-    resp.write('<th>Cep</th>');
-    resp.write('<th>Telefone</th>');
-    resp.write('<th>CNPJ</th>');
-    resp.write('<th>Email</th>');
+    resp.write('<th>Descrição do Produto</th>');
+    resp.write('<th>Fabricante</th>');
+    resp.write('<th>Codigo de Barras</th>');
+    resp.write('<th>Quantidade em Estoque</th>');
+    resp.write('<th>Preço de custo R$</th>');
+    resp.write('<th>Preço de Venda R$</th>');
+    resp.write('<th>Validade</th>');
     resp.write('</tr>');
 
-    for (let i = 0; i < listaEmpresas.length; i++) {
+    for (let i = 0; i < listaProdutos.length; i++) {
         resp.write('<tr>');
-        resp.write(`<td>${listaEmpresas[i].nome}</td>`);
-        resp.write(`<td>${listaEmpresas[i].fantasia}</td>`);
-        resp.write(`<td>${listaEmpresas[i].endereço}</td>`);
-        resp.write(`<td>${listaEmpresas[i].cidade}</td>`);
-        resp.write(`<td>${listaEmpresas[i].estado}</td>`);
-        resp.write(`<td>${listaEmpresas[i].cep}</td>`);
-        resp.write(`<td>${listaEmpresas[i].telefone}</td>`);
-        resp.write(`<td>${listaEmpresas[i].cnpj}</td>`);
-        resp.write(`<td>${listaEmpresas[i].email}</td>`);
+        resp.write(`<td>${listaProdutos[i].descricao}</td>`);
+        resp.write(`<td>${listaProdutos[i].fabricante}</td>`);
+        resp.write(`<td>${listaProdutos[i].codbarras}</td>`);
+        resp.write(`<td>${listaProdutos[i].estoque}</td>`);
+        resp.write(`<td>${listaProdutos[i].custo}</td>`);
+        resp.write(`<td>${listaProdutos[i].venda}</td>`);
+        resp.write(`<td>${listaProdutos[i].validade}</td>`);
+
     }
     resp.write('</table>');
-    resp.write('<button><a href="/CadastrodeEmpresas.html">Cadastrar nova empresa</a></button>');
+    resp.write('<button><a href="/CadastrodeProdutos.html">Cadastrar novo Poduto</a></button>');
     resp.write('<button><a href="/index.html">Voltar</a></button>');
     resp.write('<br>');
-    if(req.cookies.dataUltimoAcesso){
+    if (req.cookies.dataUltimoAcesso) {
         resp.write('<p>Ultimo acesso: ' + req.cookies.dataUltimoAcesso + '</p>');
     }
     resp.write('</body>');
